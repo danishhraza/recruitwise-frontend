9a643691c0d267e6d0694f74c9637ba6b8e54d5e
Danish Raza <danishh.raza05@gmail.com>
2025-03-29 13:58:59 +0500

    deleted old user dashboard

--- src/assets/components/UserComponents/AppliedJobs.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function AppliedJobs() {
-  return (
-    <div>AppliedJobs</div>
-  )
-}
--- src/assets/components/UserComponents/ListedJobs.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ListedJobs() {
-  return (
-    <div>ListedJobs</div>
-  )
-}
